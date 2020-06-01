b4fcba0760ee817540f4ef90cfaa2727cd9752d7
Dominique Godin <dominique.godin@umontreal.ca>
2020-06-01 11:25:30 -0400

    Minor change in sbatch.sh

--- sbatch.sh
+++ sbatch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 ##SBATCH --account=def-arguinj                          # uncomment on Beluga
 ##SBATCH --time=0-03:00     # time (DD-HH:MM)           # uncomment on Beluga
 ##SBATCH --mem=128G         # memory (per node)         # uncomment on Beluga
@@ -9,13 +10,16 @@
 #SBATCH --output=outputs/log_files/%x_%A_%a.out         # directory must exist
 #SBATCH --array=0
 
+
 export VAR=$SLURM_ARRAY_TASK_ID
 export SCRIPT_VAR
 
+
 # TRAINING ON LPS
 SIF=/opt/tmp/godin/sing_images/tf-2.1.0-gpu-py3_sing-2.6.sif
-singularity shell --nv --bind /lcg,/opt $SIF < classifier.sh $VAR $SCRIPT_VAR
-#singularity shell      --bind /lcg,/opt $SIF < presampler.sh
+singularity shell --nv --bind /lcg,/opt $SIF classifier.sh $VAR $SCRIPT_VAR
+#singularity shell      --bind /lcg,/opt $SIF presampler.sh
+
 
 # TRAINING ON BELUGA
 #SIF=/project/def-arguinj/dgodin/sing_images/tf-2.1.0-gpu-py3_sing-3.5.sif
