18fe04a1501222173348049ea9736a7e50e03d3d
Olivier <olivier.denis.1@umontreal.ca>
2020-07-15 22:43:53 -0400

    25 reps for permutation importance

--- classifier.py
+++ classifier.py
@@ -212,5 +212,5 @@ if args.results_out != '':
 
 # FEATURE IMPORTANCE
 feats = ['full'] + images + scalars
-results = feature_permutation(model, valid_sample, valid_labels, valid_probs, feats, n_rep=2)
+results = feature_permutation(model, valid_sample, valid_labels, valid_probs, feats, n_rep=25)
 plot_importances(results)
