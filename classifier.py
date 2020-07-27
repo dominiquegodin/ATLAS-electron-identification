b94214039942831ee3de0fce3690e1d2ba6927c4
Olivier <olivier.denis.1@umontreal.ca>
2020-07-27 10:40:20 -0400

    minor fix

--- classifier.py
+++ classifier.py
@@ -89,7 +89,7 @@ i = args.rm_images
 s = args.rm_scalars
 feat = ''
 if i >= 0 : images, feat = images[:i]+images[i+1:], images[i]                                                              # Removes the specified image
-elif s >= 0 == int : scalars, feat = scalars[:s]+scalars[s+1:], scalars[s]                                                 # Removes the specified scalar
+if s >= 0: scalars, feat = scalars[:s]+scalars[s+1:], scalars[s]                                                 # Removes the specified scalar
 elif args.images == 'ON' and args.scalars == 'ON': feat = 'full'
 train_var = {'images' :images  if args.images =='ON' else [], 'tracks':[],
              'scalars':scalars if args.scalars =='ON' else []}
