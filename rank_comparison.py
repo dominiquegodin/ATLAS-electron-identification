741d865658b846a92514d93d797f74c2fe9ed797
Olivier <olivier.denis.1@umontreal.ca>
2020-07-28 10:14:44 -0400

    rank comparison between methods

--- rank_comparison.py
+++ rank_comparison.py
@@ -2,22 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-files = ('outputs/2c_10m/bkg_ratio_2d/importances.pkl', 'outputs/2c_10m/match2s_2d/importances.pkl')
+feats = [
+            'em_barrel_Lr0'  , 'em_barrel_Lr1'  , 'em_barrel_Lr2'  , 'em_barrel_Lr3', 'em_barrel_Lr1_fine', 'tile_barrel_Lr1',
+            'tile_barrel_Lr2', 'tile_barrel_Lr3', 'tracks_image'    ,   'p_Eratio', 'p_Reta'   , 'p_Rhad'     , 'p_Rphi'  ,
+            'p_TRTPID' , 'p_numberOfSCTHits' , 'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1' , 'p_f3' , 'p_deltaPhiRescaled2',
+            'p_weta2' , 'p_d0'     , 'p_d0Sig'    , 'p_qd0Sig', 'p_nTracks', 'p_sct_weight_charge' , 'p_eta'   , 'p_et_calo'
+        ]
+file1 = 'outputs/2c_10m/bkg_ratio_2d/importances.pkl'
 
 imp = {}
-for f in files:
-    rwt = f.split('/')[-2][:-3] # Type of reweighting
-    imp[rwt] = []
-    with open(f,'rb') as rfp:
+imp['permutation'] = []
+with open(file1,'rb') as rfp:
+    while True:
+        try:
+            imp['permutation'].append(pickle.load(rfp))
+        except EOFError:
+            break
+imp['permutation'] = sorted(imp['permutation'], key = lambda lst: lst[1], reverse=True)
+
+imp['removal'] = []
+for feat in feats:
+    with open('removal_importance/'+feat+'/removal_importance.pkl') as rfp:
         while True:
             try:
-                imp[rwt].append(pickle.load(rfp))
+                imp['removal'].apend(pickle.load(rfp))
+                print(imp['removal'])
             except EOFError:
                 break
-    imp[rwt] = sorted(imp[rwt], key = lambda lst: lst[1], reverse=True)
+imp['removal'] = sorted(imp['removal'], key = lambda lst: lst[1], reverse=True)
 
-noRwt = imp['bkg_ratio']
-match2s = imp['match2s']
+noRwt = imp['permutation']
+match2s = imp['removal']
+
 
 data = []
 for i in range(len(noRwt)):
@@ -27,7 +43,7 @@ print(data)
 
 fig, ax = plt.subplots(1)
 
-collabel = ['importance', files[0].split('/')[-2], files[1].split('/')[-2], 'importance' ]
+collabel = ['importance', permutation, removal, 'importance' ]
 #ax.axis('tight')
 ax.axis('off')
 the_table = ax.table(cellText=data,colLabels=collabel,loc='center')
