7c1ab57e474c970f8d33e9ae6c89b697205e3cb9
Olivier <olivier.denis.1@umontreal.ca>
2020-07-29 18:30:17 -0400

    debugging

--- utils.py
+++ utils.py
@@ -882,7 +882,7 @@ def LaTeXizer(names):
     Lscalars = [r'$E_{ratio}$', r'$R_{eta}$', r'$R_{had}$', r'$R_{phi}$' , r'TRTPID' ,   r'Number of SCT hits',
                 'ndof', r'$\Delta p/p$', r'$\Delta \eta_1$', r'$f_1$'    ,  r'$f_3$' , r'$\Delta \phi _{res}$',
                 r'$w_{\eta 2}$',  r'$d_0$', r'd_0/{\sigma(d_0)}$' , r'qd0Sig'   , r'$n_{Tracks}$',
-                r'sct_weight_charge',r'$\eta$'      , r'$\p_t$', r'$E/p$'    , r'$w_{stot}', r'n_{Blayer}' ]
+                r'sct_weight_charge',r'$\eta$'      , r'$p_t$', r'$E/p$'    , r'$w_{stot}$', r'$n_{Blayer}$' ]
     Lnames = []
     for name in names:
         if name not in scalars:
