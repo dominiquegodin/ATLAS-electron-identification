443a180b003a4bed431a90002de6c6f3532f343b
Olivier <olivier.denis.1@umontreal.ca>
2020-07-29 17:20:47 -0400

    improving matrix layout

--- utils.py
+++ utils.py
@@ -876,23 +876,28 @@ def removal_bkg_rej(model,valid_probs,labels,feat,file):
 
 def LaTeXizer(names):
     scalars  = ['p_Eratio', 'p_Reta'   , 'p_Rhad'     , 'p_Rphi'  , 'p_TRTPID' , 'p_numberOfSCTHits'  ,
-    'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1'    , 'p_f3'     , 'p_deltaPhiRescaled2',
-    'p_weta2' , 'p_d0'     , 'p_d0Sig'    , 'p_qd0Sig', 'p_nTracks', 'p_sct_weight_charge',
-    'p_eta'   , 'p_et_calo', 'p_EptRatio' , 'p_wtots1', 'p_numberOfInnermostPixelHits'    ]
-    Lscalars  = [r'$E_{ratio}$', r'$R_{eta}$', r'$R_{had}$'  , r'$R_{phi}$' , r'TRTPID' , r'Number of SCT hits',
-    'ndof'  , r'$\Delta p/p$', r'$\Delta \eta_1$', r'$f_1$', r'$f_3$'   , r'$\Delta \phi _{res}$',
-    r'$w_{\eta 2}$', r'd0'  , r'd0Sig'    , r'qd0Sig', r'$n_{Tracks}$', r'sct_weight_charge',
-    r'$\eta$' , r'$\p_t$', r'$E/p$' , r'$w_{stot}', r'n_{Blayer}' ]
+                'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1'    , 'p_f3'     , 'p_deltaPhiRescaled2',
+                'p_weta2' , 'p_d0'     , 'p_d0Sig'    , 'p_qd0Sig', 'p_nTracks', 'p_sct_weight_charge',
+                'p_eta'   , 'p_et_calo', 'p_EptRatio' , 'p_wtots1', 'p_numberOfInnermostPixelHits'    ]
+    Lscalars = [r'$E_{ratio}$', r'$R_{eta}$', r'$R_{had}$', r'$R_{phi}$' , r'TRTPID' ,   r'Number of SCT hits',
+                'ndof', r'$\Delta p/p$', r'$\Delta \eta_1$', r'$f_1$'    ,  r'$f_3$' , r'$\Delta \phi _{res}$',
+                r'$w_{\eta 2}$',   r'd0'  , r'd0Sig'    , r'qd0Sig'   , r'$n_{Tracks}$',  r'sct_weight_charge',
+                r'$\eta$'      , r'$\p_t$', r'$E/p$'    , r'$w_{stot}', r'n_{Blayer}' ]
     Lnames = []
     for name in names:
+        if name not in scalars:
+            Lnames.append(name)
+            continue
         for  i,scalar in enumerate(scalars):
             if name == scalar:
                 Lname.append(Lscalars[i])
-                return Lnames
+                break
+    return Lnames
 
-def correlations(sample, dir):
+def correlations(sample, dir, LaTeX = True):
     data = pd.DataFrame(sample)
-    names = LaTeXizer(data.columns)
+    names = data.columns
+    if LaTeX: names = LaTeXizer(names)
     correlations = data.corr()
 
     # plot correlation matrix
@@ -900,16 +905,20 @@ def correlations(sample, dir):
     ax = fig.add_subplot(111)
     cax = ax.matshow(correlations, vmin=-1, vmax=1)
     fig.colorbar(cax)
+    for (i, j), z in np.ndenumerate(correlations):
+        ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
     ticks = np.arange(0,len(names),1)
     ax.set_xticks(ticks)
     ax.set_yticks(ticks)
     ax.set_xticklabels(names)
     ax.set_yticklabels(names)
     plt.xticks(rotation=90)
+    plt.tight_layout()
     plt.savefig(dir + 'corr_matrix.png')
 
     # plot scatter plot matrix
     scatter_matrix(data, figsize = (12,10))
+    plt.tight_layout()
     plt.savefig(dir + 'scatter_plot_matrix.png')
 
 
