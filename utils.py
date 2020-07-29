b9180d92dd498a72c32caf32b1189c07fe7d4009
Olivier <olivier.denis.1@umontreal.ca>
2020-07-29 18:36:11 -0400

    improving matrix layout

--- utils.py
+++ utils.py
@@ -879,10 +879,10 @@ def LaTeXizer(names):
                 'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1'    , 'p_f3'     , 'p_deltaPhiRescaled2',
                 'p_weta2' , 'p_d0'     , 'p_d0Sig'    , 'p_qd0Sig', 'p_nTracks', 'p_sct_weight_charge',
                 'p_eta'   , 'p_et_calo', 'p_EptRatio' , 'p_wtots1', 'p_numberOfInnermostPixelHits'    ]
-    Lscalars = [r'$E_{ratio}$', r'$R_{eta}$', r'$R_{had}$', r'$R_{phi}$' , r'TRTPID' ,   r'Number of SCT hits',
+    Lscalars = [r'$E_{ratio}$', r'$R_{eta}$', r'$R_{had}$', r'$R_{phi}$' , r'TRTPID' ,   r'Nb of SCT hits',
                 'ndof', r'$\Delta p/p$', r'$\Delta \eta_1$', r'$f_1$'    ,  r'$f_3$' , r'$\Delta \phi _{res}$',
-                r'$w_{\eta 2}$',  r'$d_0$', r'd_0/{\sigma(d_0)}$' , r'qd0Sig'   , r'$n_{Tracks}$',
-                r'sct_weight_charge',r'$\eta$'      , r'$p_t$', r'$E/p$'    , r'$w_{stot}$', r'$n_{Blayer}$' ]
+                r'$w_{\eta 2}$',  r'$d_0$', r'$d_0/{\sigma(d_0)}$' , r'qd0Sig'   , r'$n_{Tracks}$',
+                r'sct wt charge',r'$\eta$'      , r'$p_t$', r'$E/p$'    , r'$w_{stot}$', r'$n_{Blayer}$' ]
     Lnames = []
     for name in names:
         if name not in scalars:
@@ -917,7 +917,7 @@ def correlations(sample, dir, LaTeX = True):
     plt.savefig(dir + 'corr_matrix.png')
 
     # plot scatter plot matrix
-    scatter_matrix(data, figsize = (12,10))
+    scatter_matrix(data, figsize = (24,20))
     plt.tight_layout()
     plt.savefig(dir + 'scatter_plot_matrix.png')
 
