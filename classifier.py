a8e389bc0b0430c7b081cb7e10ac26f28c7b56cd
Olivier <olivier.denis.1@umontreal.ca>
2020-07-31 14:45:38 -0400

    path fix

--- classifier.py
+++ classifier.py
@@ -213,14 +213,15 @@ if args.correlation == 'ON':
         dir = 'QT'
     else :
         dir = 'NoQT'
-    for path in [args.output_dir + '/correlations/' + dir, args.output_dir + '/correlations/' + dir + '/signal/', args.output_dir + '/correlations/' + dir + '/bkg/']:
+    for path in [args.output_dir + '/correlations/', args.output_dir + '/correlations/' + dir,
+                 args.output_dir + '/correlations/' + dir + '/signal/', args.output_dir + '/correlations/' + dir + '/bkg/']:
         try: os.mkdir(path)
         except FileExistsError: pass
     print('CLASSIFIER : evaluating variables correlations')
     sig_sample = {key : valid_sample[key][np.where(valid_labels == 0)[0]] for key in scalars}
     bkg_sample = {key : valid_sample[key][np.where(valid_labels == 1)[0]] for key in scalars}
-    correlations(bkg_sample, args.output_dir + '/correlations/bkg/')
-    correlations(sig_sample, args.output_dir + '/correlations/signal/')
+    correlations(bkg_sample, args.output_dir + '/correlations/' + dir + '/bkg/')
+    correlations(sig_sample, args.output_dir + '/correlations/' + dir + '/signal/')
     sys.exit() # No need for training or validation
 
 
