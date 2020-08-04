c8ff5a1e5990dcc674bcc9b8473f79bf931b7e6c
Olivier <olivier.denis.1@umontreal.ca>
2020-08-04 15:33:26 -0400

    debugging

--- plot_rm_imp.py
+++ plot_rm_imp.py
@@ -2,6 +2,7 @@ import pickle
 from utils import plot_importances
 
 feats = [
+            'full',
             'em_barrel_Lr0'  , 'em_barrel_Lr1'  , 'em_barrel_Lr2'  , 'em_barrel_Lr3', 'em_barrel_Lr1_fine', 'tile_barrel_Lr1',
             'tile_barrel_Lr2', 'tile_barrel_Lr3', 'tracks_image'    ,   'p_Eratio', 'p_Reta'   , 'p_Rhad'     , 'p_Rphi'  ,
             'p_TRTPID' , 'p_numberOfSCTHits' , 'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1' , 'p_f3' , 'p_deltaPhiRescaled2',
@@ -15,13 +16,15 @@ for file in feats:
         with open('/scratch/odenis/removal_importance/' + file + '/removal_importance.pkl', 'rb') as rfp:
             bkg_tup = pickle.load(rfp)
             key = bkg_tup[0].replace(' ', '_')
+
             bkg_rej[key] = bkg_tup[1]
     except:
+        print(file + ' not in directory')
         continue
-
+print('\n', bkg_rej)
 imp = dict()
 for feat in feats[1:]:
-    imp[feat] = 1685.52/bkg_rej[feat], 0.05
+    imp[feat] = bkg_rej['full']/bkg_rej[feat], 0.05
 path = '/scratch/odenis/removal_importance/rm_imp.png'
 title = 'Feature removal importance without reweighting'
 plot_importances(imp, path, title)
