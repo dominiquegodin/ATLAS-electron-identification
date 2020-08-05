d93de5441c04d50ee3283ba2dcdbd0d828cb0e2e
Olivier <olivier.denis.1@umontreal.ca>
2020-08-05 19:30:37 -0400

    debug

--- classifier.py
+++ classifier.py
@@ -77,9 +77,15 @@ CNN = {(56,11):{'maps':[200,200], 'kernels':[ (3,3) , (3,3) ], 'pools':[ (2,2) ,
 
 # DATAFILE
 #if args.data_file == '': args.data_file = '/opt/tmp/godin/el_data/2020-05-28/el_data.h5'
-if args.data_file == ''          : args.data_file = '/scratch/odenis/el_data/0.0_1.3/el_data.h5', region = 'barrel'
-if args.data_file == 'transition': args.data_file = '/scratch/odenis/el_data/1.3_1.6/el_data.h5', region = 'transition'
-if args.data_file == 'endcap'    : args.data_file = '/scratch/odenis/el_data/1.6_2.5/el_data.h5', region = 'endcap'
+if args.data_file == ''          :
+    args.data_file = '/scratch/odenis/el_data/0.0_1.3/el_data.h5'
+    region = 'barrel'
+if args.data_file == 'transition':
+    args.data_file = '/scratch/odenis/el_data/1.3_1.6/el_data.h5'
+    region = 'transition'
+if args.data_file == 'endcap'    :
+    args.data_file = '/scratch/odenis/el_data/1.6_2.5/el_data.h5'
+    region = 'endcap'
 #for key, val in h5py.File(args.data_file, 'r').items(): print(key, val.shape)
 
 # TRAINING VARIABLES
