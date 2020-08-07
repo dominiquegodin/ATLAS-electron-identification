3479043d0e01bfd933f8c6a90e1fd1becdd09570
Olivier <olivier.denis.1@umontreal.ca>
2020-08-07 10:22:01 -0400

    reference training

--- classifier.sh
+++ classifier.sh
@@ -1,7 +1,7 @@
 # FEATURE REMOVAL IMPORTANCE (BELUGA)
 python classifier.py  --n_train=10e6  --n_valid=10e6  --batch_size=5e3  --n_epochs=100  --n_classes=2           \
                       --verbose=2  --output_dir='/scratch/odenis/removal_importance'                            \
-                      --impOut='removal_importance.pkl' --rm_features=${VAR} --data_file='endcap'
+                      --impOut='removal_importance.pkl' --data_file='endcap'
 exit
 
 
