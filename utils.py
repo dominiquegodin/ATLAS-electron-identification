7d8c9c792ba3801c3031424123f62c8fc0726310
Olivier <olivier.denis.1@umontreal.ca>
2020-08-10 15:03:08 -0400

    debug

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from   skimage    import transform
 from   plots_DG   import valid_accuracy, plot_history, plot_distributions_DG, plot_ROC_curves
 from   plots_KM   import plot_distributions_KM, differential_plots
 from   copy       import deepcopy
-rdm = tf.random
+rdm = np.random
 
 
 def find_bin(array,binning):
@@ -849,8 +849,6 @@ def feature_permutation(model, valid_sample, labels, valid_probs, feats, g , n_r
 
     for k in range(n_rep):                                                                  # Reshuffling loop
         print('PERMUTATION DE ' + features + " " + str(k+1))
-        for key in shuffled_sample:
-            shuffled_sample[key] = tf.convert_to_tensor(shuffled_sample[key])
         for feat in feats:
             rdm.shuffle(shuffled_sample[feat])                                              # Shuffling of one feature
         probs = model.predict(shuffled_sample, batch_size=20000, verbose=1)                 # Prediction with only one feature shuffled
