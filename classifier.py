84a82dbd6eb2776d6c29b0f1fe4cc52850b15804
Olivier <olivier.denis.1@umontreal.ca>
2020-08-17 16:51:46 -0400

    debug

--- classifier.py
+++ classifier.py
@@ -236,8 +236,8 @@ if args.correlation in ['ON','SCATTER']:
             valid_sample[image + '_mean'] = np.mean(valid_sample[image], axis = (1,2))
             scalars += [image + '_mean']
             fmode = '_with_im_means'
-    print(np.where(np.isfinite(valid_sample['lar_endcap_Lr0']))[0])
-    print(np.where(np.isfinite(valid_sample['lar_endcap_Lr0_mean']))[0])
+    print(np.all(np.isfinite(valid_sample['lar_endcap_Lr0'])))
+    print(np.all(np.isfinite(valid_sample['lar_endcap_Lr0_mean'])))
     sig_sample = {key : valid_sample[key][np.where(valid_labels == 0)[0]] for key in scalars}
     bkg_sample = {key : valid_sample[key][np.where(valid_labels == 1)[0]] for key in scalars}
 
