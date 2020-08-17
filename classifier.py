6195dacac989af52ed2f00a363e1a0d09008e7c5
Olivier <olivier.denis.1@umontreal.ca>
2020-08-17 17:26:19 -0400

    debug

--- classifier.py
+++ classifier.py
@@ -236,9 +236,9 @@ if args.correlation in ['ON','SCATTER']:
             valid_sample[image + '_mean'] = np.mean(valid_sample[image], axis = (1,2))
             scalars += [image + '_mean']
             fmode = '_with_im_means'
-            #print(image)
+            print(image)
             #print(np.all(np.isfinite(valid_sample[image])))
-            #print('mean : ', np.all(np.isfinite(valid_sample[image + '_mean'])))
+            print('min :', np.amin(valid_sample[image + '_mean']), 'max :', np.amax(valid_sample[image + '_mean']))
     sig_sample = {key : valid_sample[key][np.where(valid_labels == 0)[0]] for key in scalars}
     bkg_sample = {key : valid_sample[key][np.where(valid_labels == 1)[0]] for key in scalars}
 
