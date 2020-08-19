d42603348b70536baf0c99e32fd13c0dc5c0b49b
Olivier <olivier.denis.1@umontreal.ca>
2020-08-19 16:33:50 -0400

    barrel image OFF

--- classifier.sh
+++ classifier.sh
@@ -1,6 +1,6 @@
 # FEATURE REMOVAL IMPORTANCE (BELUGA)
 python classifier.py  --n_train=10e6  --n_valid=10e6  --batch_size=5e3  --n_epochs=100  --n_classes=2            \
-                      --verbose=2  --output_dir='/scratch/odenis/removal_importance/ImagesOFF' --data_file=endcap\
+                      --verbose=2  --output_dir='/scratch/odenis/removal_importance/ImagesOFF' --data_file=barrel\
                       --impOut='rm_importance.pkl' --images=OFF --removal=ON --rm_features=$VAR                \
 
 exit
