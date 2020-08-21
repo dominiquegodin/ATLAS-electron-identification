1e861e5d10c3103f7a5b22e26ec47e720c645519
Olivier <olivier.denis.1@umontreal.ca>
2020-08-21 14:46:27 -0400

    adding signal to prm sample

--- classifier.py
+++ classifier.py
@@ -325,11 +325,10 @@ if args.permutation == 'ON':
         for i in range(5):
             print('Creating sample ' + str(i))
             if not i:
-                bkg_sample.append({key:valid_sample[key][valid_labels >= 1] for key in valid_sample})
-                continue
-                bkg_labels = valid_labels[valid_labels >= 1]
+                prm_sample.append(valid_sample)
+                prm_labels = valid_labels
             else:
-                bkg_sample.append({key:valid_sample[key][valid_labels == i] for key in valid_sample})
-                bkg_labels = valid_labels[valid_labels == i]
-            feature_permutation(model, bkg_sample[i], bkg_labels, feats[args.feat], g, args.n_reps, fname + str(i))
+                prm_sample.append({key:valid_sample[key][valid_labels == i or valid_labels == 0] for key in valid_sample})
+                prm_labels = valid_labels[valid_labels == i or valid_labels == 0]
+            feature_permutation(model, prm_sample[i], prm_labels, feats[args.feat], g, args.n_reps, fname + str(i))
             print_importances(fname)
