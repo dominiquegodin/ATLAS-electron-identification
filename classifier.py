ab0f035d2bc3830924fb65bcadb7e779d235eadc
Olivier <olivier.denis.1@umontreal.ca>
2020-08-24 15:09:29 -0400

    debug

--- classifier.py
+++ classifier.py
@@ -109,7 +109,7 @@ images   = ['em_barrel_Lr0'  , 'em_barrel_Lr1'  , 'em_barrel_Lr2'  , 'em_barrel_
 #images  += ['lar_endcap_Lr0' , 'lar_endcap_Lr1' , 'lar_endcap_Lr2' , 'lar_endcap_Lr3']
 images  += ['tile_gap_Lr1']
 #images  += ['tile_barrel_Lr1', 'tile_barrel_Lr2', 'tile_barrel_Lr3']
-images  += ['tracks_image']                        ]
+images  += ['tracks_image']
 scalars  = ['p_Eratio', 'p_Reta'   , 'p_Rhad'     , 'p_Rphi'  , 'p_TRTPID' , 'p_numberOfSCTHits'  ,
             'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1'    , 'p_f3'     , 'p_deltaPhiRescaled2',
             'p_weta2' , 'p_d0'     , 'p_d0Sig'    , 'p_qd0Sig', 'p_nTracks', 'p_sct_weight_charge',
