fcbac1b48eb233ce6f69f3e3fc18390a94432352
Olivier <olivier.denis.1@umontreal.ca>
2020-08-27 14:35:32 -0400

    fix bug

--- utils.py
+++ utils.py
@@ -1002,7 +1002,7 @@ def plot_permutation(output_dir, feats, n_classes, n_reps):
     if n_classes == 2: n_bkg = 1
     else: n_bkg = n_classes
     plot = output_dir + 'prm_imp'
-    results = list(range(n_bkg))
+    results = [{} for i in range(n_bkg)]
     absent = []
     for feat in feats:
         file = output_dir + '/permutation_importance/' + feat + '_importance.pkl'
