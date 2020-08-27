d67632f525649ae195b6e4d34484e35f7b44634e
Olivier <olivier.denis.1@umontreal.ca>
2020-08-27 19:58:32 -0400

    fix typo

--- utils.py
+++ utils.py
@@ -896,7 +896,7 @@ def plot_importances(results, path, title):
                                 'em_endcap_Lr0'  , 'em_endcap_Lr1'  , 'em_endcap_Lr2'  , 'em_endcap_Lr3' , 'em_endcap_Lr1_fine',
                                 'lar_endcap_Lr0' , 'lar_endcap_Lr1' , 'lar_endcap_Lr2' , 'lar_endcap_Lr3', 'tile_gap_Lr1',
                                 'tile_barrel_Lr1', 'tile_barrel_Lr2', 'tile_barrel_Lr3'], 'indigo'),
-                'Tracks image': (['tracks_image'], 'lime',
+                'Tracks image': (['tracks_image'], 'lime'),
                 'Scalars'   : (['p_Eratio', 'p_Reta'   , 'p_Rhad'     , 'p_Rphi'  , 'p_TRTPID' , 'p_numberOfSCTHits'  ,
                                 'p_ndof'  , 'p_dPOverP', 'p_deltaEta1', 'p_f1'    , 'p_f3'     , 'p_deltaPhiRescaled2',
                                 'p_weta2' , 'p_d0'     , 'p_d0Sig'    , 'p_qd0Sig', 'p_nTracks', 'p_sct_weight_charge',
